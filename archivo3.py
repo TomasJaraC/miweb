783d73c39016e3fec6ea78373bb00aded088bc42
Tomas Jara <tomasjar63@gmail.com>
2022-09-30 18:31:10 -0300

    Agregando archivo 3

---
+++ archivo3.py
@@ -0,0 +1,12 @@
+import re
+
+def gmail_finder(mail):
+    patron = r"\b[\w.-]+@gmail.com"
+    resultado = re.search(patron,mail)
+    
+    if resultado is None:
+        return "No es gmail"
+    return resultado[0]
+
+
+print(gmail_finder("tomas_tuprofeonline@gmail.com"))
